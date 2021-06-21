cb3b34de6ccb1098d286e517963064ba82050db8
alex <alex.belova26@gmail.com>
2021-06-21 08:23:28 +0300

    работа со слайдером

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -5,45 +5,60 @@ import desktop_slide_3 from '../../images/desktop_slide_3.jpg';
  
 
 const Slider = () => {
+
+    const itemsWidth = 600;
+    const itemsCount = 3;
+    const slideToShow = 1;
+    const slideToScoll = 1;
+    const movePosition = slideToShow * itemsWidth;
+
     const slider = useRef(null);
-    const [items] = useState([1, 2, 3]);
+   
     const [prev, setPrev] = useState(true);
     const [next, setNext] = useState(false);
 
     let position = 0;
 
     const prevHandler = () => {
+        const itemsLeft = Math.abs(position) / itemsWidth;
 
-        position += 600;
-            slider.current.childNodes.forEach((element) => {
-                element.style= `transform: translateX(${position}px)`
-            })
-        
-        if (position >= 0) {
-            setPrev (true)
-        } else {
-            setPrev (false)
-        }
+        position += itemsLeft >= slideToScoll ? movePosition : itemsLeft * itemsWidth;
 
-        if (position <= -1200) {
-            setNext(true)
-            setPrev (false)
-        }
+        changePosition();
+        checkButtons();
     }
 
+    
     const nextHandler = () => {
-        
-        setPrev (false)
+        const itemsLeft = itemsCount - (Math.abs(position) + slideToShow * itemsWidth) / itemsWidth;
 
-        if (position > -1199) {
-            position -= 600
-            slider.current.childNodes.forEach((element) => {
-                element.style = `transform: translateX(${position}px)`
-                    
-            })
+        position -= itemsLeft >= slideToScoll ? movePosition : itemsLeft * itemsWidth;
+
+        changePosition();
+        checkButtons();
+    }
+
+    const changePosition = () => {
+        slider.current.childNodes.forEach((element) => {
+            element.style= `transform: translateX(${position}px)`
+        })
+
+    }
+
+    const checkButtons = () => {
+        if (position === 0) {
+            setPrev(true);
+            setNext(false);
+
         } else {
-            setNext(true)
+            setPrev(false);
         }
+
+        if (position <= -(itemsCount - slideToShow)*itemsWidth) {
+            setNext(true);
+            setPrev(false);
+        }
+
     }
 
     return (
